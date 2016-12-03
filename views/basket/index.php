4f9d96b786bf9c1da29b3bd2f03bbbb6506801bc
mmerzlyakov <alpmsm@ya.ru>
2016-12-03 23:30:07 +0700

    +functional Orders/OrdersItems

--- views/basket/index.php
+++ views/basket/index.php
@@ -11,25 +11,36 @@ $this->title = Yii::t('app', 'Baskets');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="basket-index">
+    <div class="container">
+        <BR><BR><BR>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Your Basket</h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?php //= Html::a(Yii::t('app', 'Create Basket'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+      //  'filterModel' => $searchModel,
+        'layout'=>"{items}",
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            //['class' => 'yii\grid\SerialColumn'],
+            //'id',
+            //'user_id',
+            [
+              'attribute' => 'booking_id',
+              'label' => 'Items',
+              'content'=>function($data){
+                  $booking = \app\models\Booking::find()->where('id = '.$data['booking_id'])->one();
+                  return \app\widgets\WBasketItem::widget(['model' => $booking]);
 
-            'id',
-            'user_id',
-            'booking_id',
-            'status',
+              }
 
+            ],
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+    </div>
 </div>
