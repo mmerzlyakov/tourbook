b3ee1bc85d55233aa57dd4bc85275e1fdef94c8b
mmerzlyakov <alpmsm@ya.ru>
2016-12-16 09:19:52 +0700

    + x в удалении картинок

---
+++ messages/ru/app.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Example text...' => 'Пример текста...',
+	'Language' => 'Язык',
+	'My account' => 'Мой аккаунт',
+	'Login' => 'Вход',
+	'Logout' => 'Выход',
+	'Signup' => 'Регистрация', 
+	'Basket' => 'Корзина',
+	'Russia' => 'Русский',
+	'Destination' => 'Пункт назначения',
+	'Name' => 'Название',
+	'Search' => 'Поиск',
+	'Search for sightseeing' => 'Поиск экскурсий и мероприятий',
+	'The easiest way to search and to book your vacation for all over the world' => 'Быстрый и легкий способ бронирования досуга по всему миру',
+];
+
